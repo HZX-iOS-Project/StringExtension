9aee6cf597ba0998903bc14ba531aa9be018dfe7
侯仲祥 <1418021044@qq.com>
2020-04-06 22:00:17 +0800

    no message

--- StringExtention/ViewController.swift
+++ StringExtention/ViewController.swift
@@ -27,8 +27,24 @@ class ViewController: UIViewController {
         ///倒数索引是1的字符
         print("倒数索引是1的字符：" + helloSwift.getCharReverse(index: 1))
         
-        //获取索引从2到4的字符串
+        ///获取索引从2到4的字符串
         print("索引从2到4的字符串：" + helloSwift.getString(startIndex: 2, endIndex: 4))
+        
+        ///获取从索引为2之后3个字符串
+        print("从索引为2之后3个字符串：" + helloSwift.getString(startIndex: 2, count: 3))
+        
+        ///获取索引为3之后的字符串
+        print("索引为3之后的字符串：" + helloSwift.subStringFrom(startIndex: 3))
+        
+        ///从开始位置截取到索引为3的地方停止
+        print("截取到索引为3的地方的字符串：" + helloSwift.subStringTo(endIndex: 3))
+        
+        ///获取前三个字符 这个方法不是扩展方法
+        print("前三个字符串是：" + helloSwift.prefix(3))
+        
+        ///获取后三个字符 这个方法不是扩展方法
+        print("前三个字符串是：" + helloSwift.suffix(3))
+        
     }
 
 
